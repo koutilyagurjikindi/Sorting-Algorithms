ba0e55770ff46772b282c9711aea8b05261d2448
koutilyagurjikindi <59332486+koutilyagurjikindi@users.noreply.github.com>
2020-06-01 15:25:31 +0530

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -2,16 +2,16 @@ function BinarySearch(arr,elem){
   let start = 0
   let end = arr.length - 1
   let middle=Math.floor((start+end)/2)
-  console.log(start,end,middle)
-    while(arr[middle] != elem && start<=end){
-      if(arr[middle]>elem){
+  const newarr = arr.sort(function(a,b){return a-b})
+    while(newarr[middle] != elem && start<=end){
+      if(newarr[middle]>elem){
         start = middle+1
       }else{
         end = middle-1
       }
       middle=Math.floor((start+end)/2)
     }
-  return arr[middle]===elem?middle:-1
+  return newarr[middle]===elem?middle:-1
 }
 
 BinarySearch([10,20,25,26,27,41,15],15)
